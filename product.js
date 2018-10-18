b09831840b6be5d4218bb0697792d8f870e0c97e
ShinjinE <shinjinethan@hotmail.com>
2018-10-18 23:25:01 +0000

    fourth

--- product.js
+++ product.js
@@ -2,7 +2,7 @@ angular.module('product', [])
     .controller('mainCtrl', mainCtrl);
 
 function mainCtrl($scope) {
-    $scope.product = "skis";
+    $scope.product = "Expert Powder Skis";
     $scope.brand = null; //"HEAD"
     $scope.selectedVariant = 0;
     $scope.onSale = false;
@@ -15,10 +15,10 @@ function mainCtrl($scope) {
             brand: "HEAD",
             image: "./images/headSki.jpg",
             quantity: 1,
-            details: "Get off piste with the Kore 105 and show your"
-            +"true colours. The ski is very responsive, with excellent"
-            +"damping and precise control. So you stay on top of every"
-            +"situation, even on steep or narrow sections. And the Koruba"
+            details: "Get off piste with the Kore 105 and show your "
+            +"true colors. The ski is very responsive, with excellent "
+            +"damping and precise control. So you stay on top of every "
+            +"situation, even on steep or narrow sections. And the Koruba "
             +"wood core makes it extremely light weight. Even with a "
             +"105 mm waist, it's the lightest ski of its type on the "
             +"market. And if there is no powder around, the Kore 105 will "
@@ -43,9 +43,9 @@ function mainCtrl($scope) {
             brand: "Salomon",
             image: "./images/salomonSkis.jpg",
             quantity: 1,
-            details: "QST 118 is the widest ski in the range, with full twin rocker"
-            +"to help you float in the deepest powder. You can also charge with"
-            +"confidence with the C/FX 3 layer, a unique reinforcement that"
+            details: "QST 118 is the widest ski in the range, with full twin rocker "
+            +"to help you float in the deepest powder. You can also charge with "
+            +"confidence with the C/FX 3 layer, a unique reinforcement that "
             +"delivers all the power of metal at a fraction of the weight.",
             //logo: "./images/salomonLogo.png"
         },
@@ -54,17 +54,17 @@ function mainCtrl($scope) {
             brand: "Atomic",
             image: "./images/atomicSkis.jpg",
             quantity: 1,
-            details: "Pure powder. Pure power. Pure fun. The Atomic Bent Chetler 120"
-            +"isn’t a ski for everybody – but for those skiers we do build it for,"
-            +"we’re sure there’s no better ski on the market."
-            +"The big boss of the new Bent Chetler ski family," 
-            +"the 120 is a freshie-friendly wide ride that will have you" 
-            +"hunting the steepest line and the deepest pow. HRZN Tech in the" 
-            +"tip and tail give you the most in float with 10% more surface area," 
-            +"less deflection, and better control in chop. A Light Woodcore build"
-            +"from tip to tail shaves 10% off of the ski’s weight while a Full Sidewall"
-            +"and Carbon Backbone add strength and stiffness. There’s no skier too skilled" 
-            +"for the Bent Chetler 120 – for off-piste, heli-skiing or backcountry adventures," 
+            details: "Pure powder. Pure power. Pure fun. The Atomic Bent Chetler 120 "
+            +"isn’t a ski for everybody – but for those skiers we do build it for, "
+            +"we’re sure there’s no better ski on the market. "
+            +"The big boss of the new Bent Chetler ski family, " 
+            +"the 120 is a freshie-friendly wide ride that will have you " 
+            +"hunting the steepest line and the deepest pow. HRZN Tech in the " 
+            +"tip and tail give you the most in float with 10% more surface area, " 
+            +"less deflection, and better control in chop. A Light Woodcore build "
+            +"from tip to tail shaves 10% off of the ski’s weight while a Full Sidewall "
+            +"and Carbon Backbone add strength and stiffness. There’s no skier too skilled " 
+            +"for the Bent Chetler 120 – for off-piste, heli-skiing or backcountry adventures, " 
             +"it’s going to deliver whatever you demand of it. You’ve never known stoke "
             +"until you’ve sent it on the Bent Chetler 120.",
             //logo: "./atomicLogo.jpeg"
